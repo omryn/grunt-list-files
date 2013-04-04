251cdd0818c2f93c35e2c4e889a84de9e5b10373
Omry Nachman <omri@wix.com>
2013-04-04 16:26:03 +0300

    attempt #100000004 to publish properly

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+var grunt = require('grunt');
+
+// Actually load this plugin's task(s).
+grunt.loadTasks("src/main/javascript/tasks");
--- src/main/javascript/tasks/list-files-task.js
+++ src/main/javascript/tasks/list-files-task.js
@@ -2,21 +2,17 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('list_files', function () {
             var data = this.data;
 
-            var base = process.cwd();
-            if (data.base) {
-                grunt.file.setBase(data.base);
-            }
             data.listTemplate = grunt.config.getRaw(this.name + '.' + this.target + '.listTemplate');
             data.itemTemplate = grunt.config.getRaw(this.name + '.' + this.target + '.itemTemplate');
-
             data.itemSeparator = grunt.config.getRaw(this.name + '.' + this.target + '.itemSeparator');
+
             function renderFile(file, index, array) {
                 /* global itemTemplate: true, itemSeparator: true */
                 /* global print: true */
                 var fileBreakDown = file.split('/');
-                var Class = fileBreakDown.pop().replace(/.js$/i,'');
+                var Class = fileBreakDown.pop().replace(/.js$/i, '');
                 var Package = fileBreakDown.join('.');
-                print(grunt.template.process(itemTemplate, {data: {File: file, Package:Package, Class:Class}}));
+                print(grunt.template.process(itemTemplate, {data: {File: file, Package: Package, Class: Class}}));
                 if (index !== array.length - 1) {
                     print(itemSeparator);
                 }
@@ -31,17 +27,20 @@ module.exports = function (grunt) {
             var DEFAULT_ITEM_TEMPLATE = '<%= File %>';
             var DEFAULT_ITEM_SEPARATOR = "\n";
 
+            var expandOptions = {
+                filter: 'isFile'
+            };
+            if (data.base) {
+                expandOptions.cwd = data.base;
+            }
             var model = {
-                files: grunt.file.expand(data.includes).map(unixPath),
+                files: grunt.file.expand(expandOptions, data.includes).map(unixPath),
                 listTemplate: data.listTemplate || DEFAULT_LIST_TEMPLATE,
                 itemTemplate: data.itemTemplate || DEFAULT_ITEM_TEMPLATE,
                 itemSeparator: data.itemSeparator || DEFAULT_ITEM_SEPARATOR,
                 items: RENDER_ITEMS
             };
 
-            console.log(model);
-
-            grunt.file.setBase(base);
             var list = grunt.template.process('<%= listTemplate %>', {data: model});
             grunt.file.write(data.dest, list);
 
